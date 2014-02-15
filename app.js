00aeae692bc205c75bd367684efef0302a29f72b
waynesmith <wayne@ansiblelabs.com>
2014-02-15 14:57:16 -0600

    Auth? You guys?

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-
 /**
  * Module dependencies.
  */
 
+var config = require('./config');
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var mysql = require('mysql');
 
+/*
+ * EXPRESS & ENVIRONMENT
+ */
 var app = express();
 
-// all environments
+// All environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'hjs');
@@ -25,15 +29,67 @@ app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+// Development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
+
+// Routes
 routes.configure(app);
 
+/*
+ * DATABASE
+ */
+var sql;
 
+function connectToDB() {
+	sql = mysql.createConnection(config.mysql); // Recreate the connection, since
+	// the old one cannot be reused.
 
-var server = http.createServer(app).listen(app.get('port'), function(){
-  require('socket.io').listen(server, { log: false });
-  console.log('Express server listening on port ' + app.get('port'));
+	sql.connect(function (err) {              // The server is either down
+		if (err) {                            // or restarting (takes a while sometimes).
+			console.log('error when connecting to db:', err);
+			setTimeout(connectToDB, 2000); // We introduce a delay before attempting to reconnect,
+		}                                  // to avoid a hot loop, and to allow our node script to
+	});                                    // process asynchronous requests in the meantime.
+	                                       // If you're also serving http, display a 503 error.
+	sql.on('error', function (err) {
+		console.log('db error', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+			connectToDB();                             // lost due to either server restart, or a
+		} else {                                       // connection idle timeout (the wait_timeout
+			throw err;                                 // server variable configures this)
+		}
+	});
+}
+
+connectToDB();
+
+/*
+ * AUTH
+ */
+var crypto = require('crypto');
+var sha = crypto.createHash('sha256');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		sql.query('SELECT * FROM account WHERE username = ? AND password = ?', {username: username, password: sha.update(password).digest('hex')}, function(err, rows) {
+			if (err) { return done(err); }
+
+			if(rows.length > 0) {
+				return done(null, rows[0]);
+			}
+
+			return done(null, false, { message: 'Invalid username or password.' });
+		});
+	}
+));
+
+/*
+ * SOMEBODY SET US UP THE BOMB
+ */
+var server = http.createServer(app).listen(app.get('port'), function () {
+	require('socket.io').listen(server, { log: false });
+	console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+/*
+ * Authentication pages.
+ */
+exports.configure = function (app) {
+	app.get('/register', function (req, res) {
+		res.render('register', { title: 'Register' });
+	});
+
+	app.post('/register', function (req, res) {
+		sql.query('SELECT id FROM account WHERE username = ?', [req.body.username], function (err, rows) {
+			if (err) throw err;
+
+			if (rows.length > 0) {
+				console.log('TODO User Already Exists error');
+			} else {
+				sql.query('INSERT INTO account ?', {username: req.body.username, password: sha.update(req.body.password).digest('hex')}, function (err) {
+					if (err) throw err;
+
+					console.log('TODO Successful Register');
+				});
+			}
+		});
+	});
+
+	app.post('/auth', function(req, res) {
+		passport.authenticate('local', { successRedirect: '/',
+			failureRedirect: '/auth',
+			failureFlash: true });
+	});
+}
