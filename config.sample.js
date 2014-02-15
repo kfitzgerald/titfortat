9920e005dc3bf1c1b8ed8cff116d8951d91585e0
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2014-02-15 14:52:46 -0600

    Added basic cli call to generate new wallet addresses

--- config.sample.js
+++ config.sample.js
@@ -17,6 +17,16 @@ module.exports = exports = {
         host: '127.0.0.1',
         user: 'titfortat',
         password: 'your_password_here'
-    }
+    },
+
+    /**
+     * CLI Paths
+     */
+    cli: [
+        { name: 'titcoin',  cmd: '/opt/titcoin/bin/bitcoin-cli' },
+        { name: 'tatcoin',  cmd: '/opt/tatcoin/bin/bitcoin-cli' },
+        { name: 'dogecoin', cmd: '/opt/bitcoin/bin/bitcoin-cli -conf=dogecoin.conf -datadir=/home/_your_username_/.dogecoin' },
+        { name: 'bitcoin',  cmd: '/opt/bitcoin/bin/bitcoin-cli' }
+    ]
 
 };
