4c425302c8d4ebe01b31eaae938e51c830ae2795
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2014-02-15 12:22:32 -0600

    Added sample app config

---
+++ config.sample.js
@@ -0,0 +1,22 @@
+
+module.exports = exports = {
+
+    /**
+     * Redis connection info
+     */
+    redis: {
+        port: 6379,
+        host: '127.0.0.1'
+        //auth: ''
+    },
+
+    /**
+     * MySQL Connection info
+     */
+    mysql: {
+        host: '127.0.0.1',
+        user: 'titfortat',
+        password: 'your_password_here'
+    }
+
+};
