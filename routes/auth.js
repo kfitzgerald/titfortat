b2b40cf0d273b7bc13634d820a26615c53022f76
waynesmith <wayne@ansiblelabs.com>
2014-02-15 15:28:19 -0600

    Maybe this? Jesus Christ.

--- routes/auth.js
+++ routes/auth.js
@@ -7,13 +7,13 @@ exports.configure = function (app) {
 	});
 
 	app.post('/register', function (req, res) {
-		sql.query('SELECT id FROM account WHERE username = ?', [req.body.username], function (err, rows) {
+		app.sql.query('SELECT id FROM account WHERE username = ?', [req.body.username], function (err, rows) {
 			if (err) throw err;
 
 			if (rows.length > 0) {
 				console.log('TODO User Already Exists error');
 			} else {
-				sql.query('INSERT INTO account ?', {username: req.body.username, password: sha.update(req.body.password).digest('hex')}, function (err) {
+				app.sql.query('INSERT INTO account ?', {username: req.body.username, password: sha.update(req.body.password).digest('hex')}, function (err) {
 					if (err) throw err;
 
 					console.log('TODO Successful Register');
@@ -23,7 +23,7 @@ exports.configure = function (app) {
 	});
 
 	app.post('/login', function(req, res) {
-		passport.authenticate('local', { successRedirect: '/',
+		app.passport.authenticate('local', { successRedirect: '/',
 			failureRedirect: '/login',
 			failureFlash: true });
 	});
