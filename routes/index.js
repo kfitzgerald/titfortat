31697dbc09a81205daad9e1158ac088337bc1d0e
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2014-02-15 17:18:32 -0600

    Added basic order handler

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,43 @@ exports.configure = function(app){
     });
 
 	require('./auth').configure(app);
+
+    app.get('/test', function(req, res) {
+        var Order = require('./../lib/order'),
+            order = new Order(app);
+
+//        // CREATE TEST
+//        order.create({
+//            account_id: 1,
+//            type: 'BUY',
+//            have_type: 'bitcoin',
+//            want_type: 'titcoin',
+//            status: Order.Status.Open,
+//            amount: 1234.00000056,
+//            rate: 1
+//        }, function(err, result) {
+//
+//            console.log('CREATE RESPONSE WAS', err, result);
+//
+//        });
+
+//        // GET AND UPDATE TEST
+//        order.get(1, function(err, data) {
+//            console.log('I CAN HAZ ORDER?', data);
+//
+//            order.update(data.id, {
+//                amount: data.amount + 1
+//            }, function(err, result){
+//                console.log('UPDATE WAS', result);
+//            })
+//        });
+
+//        // DELETE TEST
+//        order.remove(2, function(err, result) {
+//
+//            console.log('DELETE WAS', result);
+//
+//        });
+
+    });
 };
