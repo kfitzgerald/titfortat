1e03e45e05b274fbd7932550e0e50e356948465c
Matt Lea <mlea@schematical.com>
2014-02-15 18:25:42 -0600

    Glorious

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ window.tit = tit = {
         this.socket.on('account.create', function (data) {
             console.log(data);
         });
+        this.account = 1;//        'ass';//Fake it till you make it
 
     },
     db:function(model, action, data){
@@ -13,8 +14,43 @@ window.tit = tit = {
             action:action,
             data:data
         });
+    },
+    init_account:function(){
+        $('.btn-save-address').click(function(){
+            var jColl = $('#txt-address');
+            for(var i = 0; i < jColl.length; i++){
+                var jEle = $(jColl[i]);
+                tit.db('address', 'create', {
+                    account_id:this.account,
+                    type:jEle.attr('data-type'),
+                    hash:jEle.val()
+                })
+            }
+        });
+    },
+    init_orders:function(){
+
+       this.socket.on('order.get_by_account', function(orders){
+            console.log(orders);
+       })
+       this.db('order', 'get_by_account', this.account);
+    },
+    render_orders:function(orders){
+        var jBuyEle = $('#buy-orders');
+        var jSellEle = $('#sell-orders');
+        jBuyEle.empty();
+        jSellEle.empty();
+        for(var i in orders){
+            var html = '<li><a class="list-group-item">' + orders[i].ammount + ' - ' + orders[i].rate + '</a></li>';
+            if(orders[i].type == "BUY"){
+                jBuyEle.append(html);
+            }else{
+                jSellEle.append(html);
+            }
+        }
     }
 }
 $(function(){
     tit.intit();
+    tit.init_orders();
 })
