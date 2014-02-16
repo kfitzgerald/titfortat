db966651e27c6fdea44bf15bb55d8ef3a7632605
waynesmith <wayne@ansiblelabs.com>
2014-02-15 18:02:58 -0600

    Register still broke.

--- routes/auth.js
+++ routes/auth.js
@@ -8,26 +8,34 @@ var crypto = require('crypto'),
 
 exports.configure = function (app) {
     var sql = app.get('sql');
-    console.log('SQL IS', sql);
+    // console.log('SQL IS', sql);
 	app.get('/register', function (req, res) {
 		res.render('register', { title: 'Register' });
 	});
 
 	app.post('/register', function (req, res) {
+		console.log(1);
         sql.query('SELECT id FROM account WHERE username = ?', [req.body.username], function (err, rows) {
 			if (err) throw err;
+
+	        console.log(2);
 
 			if (rows.length > 0) {
 				console.log('TODO User Already Exists error');
 			} else {
+				console.log(3);
                 // console.log(req, req.body);
                 sql.query('INSERT INTO account SET ?', { username: req.body.username, password: sha.update(req.body.password).digest('hex') }, function (err, result) {
 					if (err) throw err;
+
+	                console.log(4);
 
                     var newAccountId = result.insertId;
 
                     var cli = new BitCli(app.get('config'));
                     cli.generateNewAddressSet(function(err, addresses) {
+
+	                    console.log(5);
 
                         /* { titcoin: 'tFDwzanzueEY6huHh8SsKCWCAyXqiADc6A',
                          tatcoin: 'TQcWvBWDgJgjZ7cu9GY1tX9a4quDNi4nAo',
@@ -45,13 +53,22 @@ exports.configure = function (app) {
                         sql.query('INSERT INTO address (account_id, type, hash) VALUES ?', [values], function(err) {
                             if (err) throw err;
 
+	                        console.log(6);
+
+	                        /*
+	                         * Duping the login code because I suck at learning
+	                         */
 	                        sql.query('SELECT * FROM account WHERE id = ?', [newAccountId], function(err, rows) {
 		                        if (err) throw err;
+
+		                        console.log(7);
 
 		                        var user = rows[0];
 
 		                        sql.query('SELECT * FROM address WHERE account_id = ?', [newAccountId], function(err, rows) {
 			                        if (err) throw err;
+
+			                        console.log(8);
 
 			                        user.addresses = rows;
 
@@ -69,6 +86,7 @@ exports.configure = function (app) {
 	});
 
 	app.post('/login', function(req, res) {
+
 		var passport = app.get('passport');
         passport.authenticate('local', { successRedirect: '/account',
 			failureRedirect: '/login',
